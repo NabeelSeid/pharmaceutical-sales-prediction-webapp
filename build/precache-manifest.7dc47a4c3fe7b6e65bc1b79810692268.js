7eb252684a546cbb5358e3b728a55b7929f1afc9
Nabeel Seid <nabeelseid@gmail.com>
2020-08-22 16:04:01 +0300

    Add files via upload

---
+++ build/precache-manifest.7dc47a4c3fe7b6e65bc1b79810692268.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a40153761d6266a2e86d80b5fe2f26c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "29e05410a0b49f2b8e73",
+    "url": "/static/css/main.32846b1c.chunk.css"
+  },
+  {
+    "revision": "f06b905c83a12caec5f9",
+    "url": "/static/js/2.f5c1d5e6.chunk.js"
+  },
+  {
+    "revision": "c836aaadfe43dfd9fcb8ddeddfd61e18",
+    "url": "/static/js/2.f5c1d5e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29e05410a0b49f2b8e73",
+    "url": "/static/js/main.62a867a4.chunk.js"
+  },
+  {
+    "revision": "4dddae754c89068dc0b0",
+    "url": "/static/js/runtime-main.ca770073.js"
+  }
+]);
